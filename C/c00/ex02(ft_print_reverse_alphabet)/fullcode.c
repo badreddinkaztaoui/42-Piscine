0d2f931acb807eb905ea224121c865a51f3eb173
alaamimi <aku-no-hana@outlook.fr>
2021-06-02 18:58:57 +0100

    ft_print_reverse_alphabet

--- C/c00/ex02(ft_print_reverse_alphabet)/fullcode.c
+++ C/c00/ex02(ft_print_reverse_alphabet)/fullcode.c
@@ -6,31 +6,31 @@
 /*   By: alaamimi <alaamimi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/02 18:30:42 by alaamimi          #+#    #+#             */
-/*   Updated: 2021/06/02 18:35:27 by alaamimi         ###   ########.fr       */
+/*   Updated: 2021/06/02 18:56:33 by alaamimi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
+#include <unistd.h> /* Library that contains the system call write */
 #include <stdlib.h>
 #include <stdio.h>
 
-void	ft_print_reverse_alphabet(void)
+void	ft_print_reverse_alphabet(void) /* Define a function that will reverse alphabets */
 {
-	char	ltr;
+	char	ltr; /* Declaration of a variable that will contain a character */
 
-	ltr = 'z';
-	while (ltr >= 'a')
+	ltr = 'z'; /* Initialize my variable with z */
+	while (ltr >= 'a') /* Loop through ascii values till the stop condition */
 	{
-		write(1, &ltr, 1);
-		ltr--;
+		write(1, &ltr, 1); /* Write evert character passed as parameter after checking the condition */
+		ltr--; /* Decrement till the condition is satisfied */
 	}
 }
 
-int	main(void)
+int	main(void) /* Entry point */
 {
 	printf("----------------------------------\n");
 	printf("Print reversed aphabets in the standard output : \n\n");
-	ft_print_reverse_alphabet();
+	ft_print_reverse_alphabet(); /*This function takes no parameters*/
 	printf("\n\n----------------------------------\n");
 	return (EXIT_SUCCESS);
 }
